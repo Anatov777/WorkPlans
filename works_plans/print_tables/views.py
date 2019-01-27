7ec2dc226bc3ec64fe5faa09e25b9af2c7fc55f0
Timur <anatovtimur@gmail.com>
2019-01-27 09:34:47 +0300

    changed print tables for admin

--- works_plans/print_tables/views.py
+++ works_plans/print_tables/views.py
@@ -3,27 +3,24 @@ from plan_tables.models import *
 from django.contrib import auth
 
 
-def setArgsPrintTables(querySetName, modelName, teacherUsername, indPlanObject, request, args):
-    try:
-        if teacherUsername != None:
-            args[querySetName+'QuerySet'] = modelName.objects.filter(usern=teacherUsername)
-            args['selectTeacher'] = teacher.objects.filter(usern=teacherUsername)
-        else:
-            args[querySetName+'QuerySet'] = modelName.objects.filter(usern=auth.get_user(request).username, planID=indPlanObject).order_by('num_p')
-    except modelName.DoesNotExist:
-        args[querySetName+'QuerySet'] = None
-
 def fall_table(request, plan_id, year_id):
 
     args = {}
+
+    if request.GET.get('chooseTeacher'):
+        s = request.GET.get('chooseTeacher')
+        s = s.split(',')
+        teacherId = s[2].replace(' ', '')
+    args['selectTeacher'] = teacher.objects.filter(teacherID=teacherId)
     args['username'] = auth.get_user(request).username
     args['teacher'] = teacher.objects.filter(usern=auth.get_user(request).username)
 
     args['argSelectedPlanId'] = int(plan_id)
     args['argSelectedYear'] = year_id.replace('_','/')
+    args['choosedTeacher'] = request.GET.get('chooseTeacher')
 
-    teacherUsername = None
-
+    teacherUsername = teacher.objects.get(teacherID=teacherId)
+    teacherUsername = teacherUsername.usern
 
     teacherIdObject = teacher.objects.filter(usern=auth.get_user(request).username)
     for e in teacherIdObject:
@@ -34,15 +31,14 @@ def fall_table(request, plan_id, year_id):
 
     indPlanObject = indPlan.objects.get(planID=int(plan_id), year=year_id.replace('_','/'))
 
-    setArgsPrintTables('fallWork', plansFall, teacherUsername, indPlanObject, request, args)
-    setArgsPrintTables('fallHourlyWork', plansFallHourly, teacherUsername, indPlanObject, request, args)
-    setArgsPrintTables('springWork', plansSpring, teacherUsername, indPlanObject, request, args)
-    setArgsPrintTables('springHourlyWork', plansSpringHourly, teacherUsername, indPlanObject, request, args)
-    setArgsPrintTables('educMethodWork', educMethodPlan, teacherUsername, indPlanObject, request, args)
-    setArgsPrintTables('sciWork', sciPlan, teacherUsername, indPlanObject, request, args)
-    setArgsPrintTables('litWork', litPlan, teacherUsername, indPlanObject, request, args)
-    setArgsPrintTables('otherWork', otherPlan, teacherUsername, indPlanObject, request, args)
-    setArgsPrintTables('trainingWork', trainingPlan, teacherUsername, indPlanObject, request, args)
-
+    args['fallWorkQuerySet'] = plansFall.objects.filter(usern=teacherUsername, planID=indPlanObject).order_by('num_p')
+    args['fallHourlyWorkQuerySet'] = plansFallHourly.objects.filter(usern=teacherUsername, planID=indPlanObject).order_by('num_p')
+    args['springWorkQuerySet'] = plansSpring.objects.filter(usern=teacherUsername, planID=indPlanObject).order_by('num_p')
+    args['springHourlyWorkQuerySet'] = plansSpringHourly.objects.filter(usern=teacherUsername, planID=indPlanObject).order_by('num_p')
+    args['educMethodWorkQuerySet'] = educMethodPlan.objects.filter(usern=teacherUsername, planID=indPlanObject).order_by('num_p')
+    args['sciWorkQuerySet'] = sciPlan.objects.filter(usern=teacherUsername, planID=indPlanObject).order_by('num_p')
+    args['litWorkQuerySet'] = litPlan.objects.filter(usern=teacherUsername, planID=indPlanObject).order_by('num_p')
+    args['otherWorkQuerySet'] = otherPlan.objects.filter(usern=teacherUsername, planID=indPlanObject).order_by('num_p')
+    args['trainingWorkQuerySet'] = trainingPlan.objects.filter(usern=teacherUsername, planID=indPlanObject).order_by('num_p')
 
     return render(request, 'print_tables/print_table.html', args)
